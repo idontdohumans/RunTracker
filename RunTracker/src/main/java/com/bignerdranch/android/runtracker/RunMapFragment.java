050b2fc418bd7c4dbb74914b084e81a721da842c
Donald <donald@thehoefer.com>
2013-07-26 15:35:13 -0400

    runMapFragment can now stash a LocationCursor to hold on to the list of locations.

--- RunTracker/src/main/java/com/bignerdranch/android/runtracker/RunMapFragment.java
+++ RunTracker/src/main/java/com/bignerdranch/android/runtracker/RunMapFragment.java
@@ -1,6 +1,9 @@
 package com.bignerdranch.android.runtracker;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +14,13 @@ import com.google.android.gms.maps.SupportMapFragment;
 /**
  * Created by panda on 7/26/13.
  */
-public class RunMapFragment extends SupportMapFragment {
+public class RunMapFragment extends SupportMapFragment
+        implements LoaderManager.LoaderCallbacks<Cursor>{
     private static final String ARG_RUN_ID = "RUN_ID";
+    private static final int LOAD_LOCATIONS = 0;
 
     private GoogleMap mGoogleMap;
+    private RunDatabaseHelper.LocationCursor mLocationCursor;
 
     public static RunMapFragment newInstance(long runId) {
         Bundle args = new Bundle();
@@ -22,6 +28,39 @@ public class RunMapFragment extends SupportMapFragment {
         RunMapFragment rf = new RunMapFragment();
         rf.setArguments(args);
         return rf;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Check for a Run ID as an argument, and find the run
+        Bundle args = getArguments();
+        if (args != null) {
+            long runId = args.getLong(ARG_RUN_ID, -1);
+            if (runId != -1) {
+                LoaderManager lm = getLoaderManager();
+                lm.initLoader(LOAD_LOCATIONS, args, this);
+            }
+        }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        long runId = args.getLong(ARG_RUN_ID, -1);
+        return new LocationListCursorLoader(getActivity(), runId);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        mLocationCursor = (RunDatabaseHelper.LocationCursor)cursor;
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // Stop using the data
+        mLocationCursor.close();
+        mLocationCursor = null;
     }
 
     @Override
